5b7f568df126c7e81426f415fa1c31a6e46b4a66
liqi7 <liqi7@lenovo.com>
2018-10-09 18:22:19 +0800

    second finish

---
+++ config.py
@@ -0,0 +1,5 @@
+"""
+    Created by Lucky on 2018/10/9 18:15
+"""
+
+DEBUG = True
--- fisher.py
+++ fisher.py
@@ -1,18 +1,27 @@
 """
     Created by Lucky on 2018/10/9 15:48
 """
-from flask import Flask
+from flask import Flask, make_response
 
 __author__ = "lucky"
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/hello')
 def hello():
-    return "Hello, Lucky"
+    headers = {
+        'content-type': 'application/json',
+        'location': 'http://www.bing.com'
+    }
+    # response = make_response('<html></html>', 301)
+    # response.headers = headers
+    return '<html></html>', 301, headers
 
 
 # app.add_url_rule("/hello", view_func=hello)
 
-app.run(host='0.0.0.0', debug=True,port='')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81)
